60a25434b89e5e1eb583b1a23e675fd99ae4f322
Michael Kutzer <kutzer@usna.edu>
2020-03-29 13:54:01 -0400

    setupTurretAndTaget working

--- computer_vision/SCRIPT_Test_setupTurretAndTarget.m
+++ computer_vision/SCRIPT_Test_setupTurretAndTarget.m
@@ -13,75 +13,232 @@ saveMedia = false;
 h = createEW309RoomFOV('Ri080');
 
 %% Define test parameters
-walls = {'NE','NW','SW','SE'};
-ranges = linspace(5,20,100).*(12*25.4);             % Test ranges
-hOffsets = linspace(-10,10,100).*(12*25.4);         % Horizontal offsets
-vOffsets = linspace(-3,3,100).*(12*25.4);           % Vertical offsets
-thetas = linspace( deg2rad(-12),deg2rad(12),100 );   % Turret angles
+walls    = {'NE','NW','SW','SE'};
+ranges   = linspace(5,20,100).*(12*25.4);               % Test ranges
+hBiases  = linspace(-10,10,100).*(12*25.4);             % Horizontal bias
+vBiases  = linspace(-3,3,100).*(12*25.4);               % Vertical bias
+hOffsets = linspace(-10,10,100).*(12*25.4);             % Horizontal offset
+vOffsets = linspace(-3,3,100).*(12*25.4);               % Vertical offset
+thetas   = linspace( deg2rad(-12),deg2rad(12),100 );    % Turret angles
 
 %% Test walls
+hBias = 0;
+vBias = -1*12*25.4;
 hOffset = 0;
-vOffset = -1*12*25.4;
+vOffset = 0;
 theta = 0;
 range = 15*12*25.4;
 for w = 1:numel(walls)
     wall = walls{w};
     
+    % Define target specs
     targetSpecs.Diameter = 5*25.4;
-    targetSpecs.VerticalOffset = vOffset;
-    targetSpecs.HorizontalOffset = hOffset;
+    targetSpecs.HorizontalBias = hBias;
+    targetSpecs.VerticalBias = vBias;
     targetSpecs.Color = 'Dark Orange';
     targetSpecs.Wobble = 0;
+    
+    % Define turret specs
+    turretSpecs.HorizontalOffset = hOffset;
+    turretSpecs.VerticalOffset = vOffset;
+    turretSpecs.Angle = theta;
     
     if exist('h_a2r')
         delete(h_a2r);
     end
-    
-    [h,h_a2r] = setupTurretAndTarget(h,targetSpecs,range,theta,wall);
+    [h,h_a2r] = setupTurretAndTarget(h,targetSpecs,range,turretSpecs,wall);
     drawnow;
     pause;
 end
 
 %% Test walls and range shifts
+hBias = 0;
+vBias = -1*12*25.4;
 hOffset = 0;
-vOffset = -1*12*25.4;
+vOffset = 0;
 theta = 0;
+%range = 15*12*25.4;
 for w = 1:numel(walls)
     wall = walls{w};
+    
     for range = ranges
+        % Define target specs
         targetSpecs.Diameter = 5*25.4;
-        targetSpecs.VerticalOffset = vOffset;
-        targetSpecs.HorizontalOffset = hOffset;
+        targetSpecs.HorizontalBias = hBias;
+        targetSpecs.VerticalBias = vBias;
         targetSpecs.Color = 'Dark Orange';
         targetSpecs.Wobble = 0;
+        
+        % Define turret specs
+        turretSpecs.HorizontalOffset = hOffset;
+        turretSpecs.VerticalOffset = vOffset;
+        turretSpecs.Angle = theta;
         
         if exist('h_a2r')
             delete(h_a2r);
         end
-        
-        [h,h_a2r] = setupTurretAndTarget(h,targetSpecs,range,theta,wall);
+        [h,h_a2r] = setupTurretAndTarget(h,targetSpecs,range,turretSpecs,wall);
         drawnow;
+
     end
 end
 
-%% Test walls and angle shifts
+%% Test walls and theta shifts
+hBias = 0;
+vBias = -1*12*25.4;
 hOffset = 0;
-vOffset = -1*12*25.4;
+vOffset = 0;
+%theta = 0;
 range = 15*12*25.4;
 for w = 1:numel(walls)
     wall = walls{w};
+    
     for theta = thetas
+        % Define target specs
         targetSpecs.Diameter = 5*25.4;
-        targetSpecs.VerticalOffset = vOffset;
-        targetSpecs.HorizontalOffset = hOffset;
+        targetSpecs.HorizontalBias = hBias;
+        targetSpecs.VerticalBias = vBias;
         targetSpecs.Color = 'Dark Orange';
         targetSpecs.Wobble = 0;
+        
+        % Define turret specs
+        turretSpecs.HorizontalOffset = hOffset;
+        turretSpecs.VerticalOffset = vOffset;
+        turretSpecs.Angle = theta;
         
         if exist('h_a2r')
             delete(h_a2r);
         end
-        
-        [h,h_a2r] = setupTurretAndTarget(h,targetSpecs,range,theta,wall);
+        [h,h_a2r] = setupTurretAndTarget(h,targetSpecs,range,turretSpecs,wall);
         drawnow;
+
+    end
+end
+
+%% Test walls and vertical offset shifts
+hBias = 0;
+vBias = -1*12*25.4;
+hOffset = 0;
+%vOffset = 0;
+theta = 0;
+range = 15*12*25.4;
+for w = 1:numel(walls)
+    wall = walls{w};
+    
+    for vOffset = vOffsets
+        % Define target specs
+        targetSpecs.Diameter = 5*25.4;
+        targetSpecs.HorizontalBias = hBias;
+        targetSpecs.VerticalBias = vBias;
+        targetSpecs.Color = 'Dark Orange';
+        targetSpecs.Wobble = 0;
+        
+        % Define turret specs
+        turretSpecs.HorizontalOffset = hOffset;
+        turretSpecs.VerticalOffset = vOffset;
+        turretSpecs.Angle = theta;
+        
+        if exist('h_a2r')
+            delete(h_a2r);
+        end
+        [h,h_a2r] = setupTurretAndTarget(h,targetSpecs,range,turretSpecs,wall);
+        drawnow;
+
+    end
+end
+
+%% Test walls and horizontal offset shifts
+hBias = 0;
+vBias = -1*12*25.4;
+%hOffset = 0;
+vOffset = 0;
+theta = 0;
+range = 15*12*25.4;
+for w = 1:numel(walls)
+    wall = walls{w};
+    
+    for hOffset = hOffsets
+        % Define target specs
+        targetSpecs.Diameter = 5*25.4;
+        targetSpecs.HorizontalBias = hBias;
+        targetSpecs.VerticalBias = vBias;
+        targetSpecs.Color = 'Dark Orange';
+        targetSpecs.Wobble = 0;
+        
+        % Define turret specs
+        turretSpecs.HorizontalOffset = hOffset;
+        turretSpecs.VerticalOffset = vOffset;
+        turretSpecs.Angle = theta;
+        
+        if exist('h_a2r')
+            delete(h_a2r);
+        end
+        [h,h_a2r] = setupTurretAndTarget(h,targetSpecs,range,turretSpecs,wall);
+        drawnow;
+
+    end
+end
+
+%% Test walls and vertical bias shifts
+hBias = 0;
+%vBias = -1*12*25.4;
+hOffset = 0;
+vOffset = 0;
+theta = 0;
+range = 15*12*25.4;
+for w = 1:numel(walls)
+    wall = walls{w};
+    
+    for vBias = vBiases
+        % Define target specs
+        targetSpecs.Diameter = 5*25.4;
+        targetSpecs.HorizontalBias = hBias;
+        targetSpecs.VerticalBias = vBias;
+        targetSpecs.Color = 'Dark Orange';
+        targetSpecs.Wobble = 0;
+        
+        % Define turret specs
+        turretSpecs.HorizontalOffset = hOffset;
+        turretSpecs.VerticalOffset = vOffset;
+        turretSpecs.Angle = theta;
+        
+        if exist('h_a2r')
+            delete(h_a2r);
+        end
+        [h,h_a2r] = setupTurretAndTarget(h,targetSpecs,range,turretSpecs,wall);
+        drawnow;
+
+    end
+end
+
+%% Test walls and horizontal bias shifts
+%ccc = 0;
+vBias = -1*12*25.4;
+hOffset = 0;
+vOffset = 0;
+theta = 0;
+range = 15*12*25.4;
+for w = 1:numel(walls)
+    wall = walls{w};
+    
+    for hBias = hBiases
+        % Define target specs
+        targetSpecs.Diameter = 5*25.4;
+        targetSpecs.HorizontalBias = hBias;
+        targetSpecs.VerticalBias = vBias;
+        targetSpecs.Color = 'Dark Orange';
+        targetSpecs.Wobble = 0;
+        
+        % Define turret specs
+        turretSpecs.HorizontalOffset = hOffset;
+        turretSpecs.VerticalOffset = vOffset;
+        turretSpecs.Angle = theta;
+        
+        if exist('h_a2r')
+            delete(h_a2r);
+        end
+        [h,h_a2r] = setupTurretAndTarget(h,targetSpecs,range,turretSpecs,wall);
+        drawnow;
+
     end
 end
--- computer_vision/setupTurretAndTarget.m
+++ computer_vision/setupTurretAndTarget.m
@@ -4,15 +4,19 @@ function varargout = setupTurretAndTarget(h,varargin)
 %
 %   h = SETUPTURRETANDTARGET(h,targetSpecs,range)
 %
-%       targetSpecs.Diameter - target diameter
-%       targetSpecs.VerticalOffset - change in height 
-%       targetSpecs.HorizontalOffset - change in left/right position
-%       targetSpecs.Color
-%       targetSpecs.Wobble - target wobble
+%       targetSpecs.Diameter ------- target diameter
+%       targetSpecs.HorizontalBias - change in left/right relative position
+%       targetSpecs.VerticalBias --- change in relative height 
+%       targetSpecs.Color ---------- target color [Dark Orange]
+%       targetSpecs.Wobble --------- target wobble [0]
 %
-%   h = SETUPTURRETANDTARGET(h,targetSpecs,range,turretAng)
+%   h = SETUPTURRETANDTARGET(h,targetSpecs,range,turretSpecs)
 %
-%   h = SETUPTURRETANDTARGET(h,targetSpecs,range,turretAng,wall)
+%       turretSpecs.HorizontalOffset - change in left/right position
+%       turretSpecs.VerticalBias ----- change in height
+%       turretSpecs.Angle ------------ change in angle
+%
+%   h = SETUPTURRETANDTARGET(h,targetSpecs,range,turretSpecs,wall)
 %
 %       wall = {['NE'],'NW','SW','SE'};
 %
@@ -24,7 +28,6 @@ function varargout = setupTurretAndTarget(h,varargin)
 narginchk(3,5);
 
 % Set defaults
-turretAng = 0;
 wall = 'NE';
 wobble = deg2rad(10); % TODO - allow user to specify target wobble
 color = 'Light Orange';
@@ -39,7 +42,7 @@ if nargin > 2
 end
 
 if nargin > 3
-    turretAng = varargin{3};
+    turretSpecs = varargin{3};
 end
 
 if nargin > 4
@@ -47,7 +50,11 @@ if nargin > 4
 end
 
 %% Check target specs
-fieldNames = {'Diameter','VerticalOffset','HorizontalOffset','Color','Wobble'};
+if ~isstruct(targetSpecs)
+    warning('"targetSpecs" must be defined as a structured array.'); 
+    targetSpecs = struct;
+end
+fieldNames = {'Diameter','HorizontalBias','VerticalBias','Color','Wobble'};
 bin = isfield(targetSpecs,fieldNames);
 for i = 1:3
     if ~bin(i)
@@ -61,16 +68,97 @@ if ~bin(5)
     targetSpecs.Wobble = wobble;
 end
 
+%% Check turret specs
+if ~isstruct(turretSpecs)
+    warning('"turretSpecs" must be defined as a structured array.'); 
+    turretSpecs = struct;
+end
+fieldNames = {'HorizontalOffset','VerticalOffset','Angle'};
+bin = isfield(turretSpecs,fieldNames);
+if ~bin(1)
+    turretSpecs.HorizontalOffset = 0;
+end
+if ~bin(2)
+    turretSpecs.VerticalOffset = 0;
+end
+if ~bin(3)
+    turretSpecs.Angle = 0;
+end
+
 %% Specify position & orientation of target and turret
 barrelLength = 356; % Offset between the barrel frame origin and the end 
                     % of the barrel
+%
+% Review frame definitions
+%   Frame c - camera frame 
+%   Frame r - room center frame
+%   Frame b - barrel frame
+%   Frame a - target "aim" frame
+%   Frame e - barrel "end" frame 
 
+% Define transformations
+% -> Barrel "end" frame relative to barrel frame
+H_e2b = Ty(barrelLength);
+% -> Target "aim" frame relative to room frame 
+switch wall
+    case 'NE'
+        target.XYZ(1) =  (targetSpecs.HorizontalBias + turretSpecs.HorizontalOffset);
+        target.XYZ(2) =  (h.Room_Length/2 - targetSpecs.Diameter/2);
+        target.theta = 0;
+    case 'NW'
+        target.XYZ(1) = -(h.Room_Width/2 - targetSpecs.Diameter/2);
+        target.XYZ(2) =  (targetSpecs.HorizontalBias + turretSpecs.HorizontalOffset);
+        target.theta = pi/2;
+    case 'SW'
+        target.XYZ(1) = -(targetSpecs.HorizontalBias + turretSpecs.HorizontalOffset);
+        target.XYZ(2) = -(h.Room_Length/2 - targetSpecs.Diameter/2);
+        target.theta = pi;
+    case 'SE'
+        target.XYZ(1) =  (h.Room_Width/2 - targetSpecs.Diameter/2);
+        target.XYZ(2) = -(targetSpecs.HorizontalBias + turretSpecs.HorizontalOffset);
+        target.theta = 3*pi/2;
+    otherwise
+        error('Specified wall "%s" is undefined.',wall);
+end
+target.XYZ(3) =  (targetSpecs.VerticalBias + turretSpecs.VerticalOffset);
+target.diameter = targetSpecs.Diameter;
+target.color = targetSpecs.Color;
+target.wobble = targetSpecs.Wobble;
+h_a2r = createTarget(h.Frames.h_r2b,'Circle',target.diameter,target.color);
+h_a2r = placeTarget(h_a2r,target.XYZ,target.theta,target.wobble);
+H_a2r = get(h_a2r,'Matrix');
+% -> Barrel "end" frame relative to target "aim" frame
+H_e2a = Tz(range)*Tx(-targetSpecs.HorizontalBias)*Ty(-targetSpecs.VerticalBias)*Rx(-pi/2);
+% -> Barrel frame relative to the room frame
+H_b2e = H_e2b^(-1);
+H_b2r = H_a2r*H_e2a*H_b2e*Rz(turretSpecs.Angle);
+
+% Move turret
+H_r2b = H_b2r^(-1);
+h.H_r2b = H_r2b;
+set(h.Frames.h_r2b,'Matrix',H_r2b);
+
+
+
+%% Package Output
+if nargout > 0
+    varargout{1} = h;
+end
+if nargout > 1
+    varargout{2} = h_a2r;
+end
+
+%%
+return
+%%
+
+%%
 % Define turret position
 switch wall
     case 'NE'
-        target.XYZ(1) = targetSpecs.HorizontalOffset;
+        target.XYZ(1) = targetSpecs.HorizontalBias;
         target.XYZ(2) = h.Room_Length/2 - targetSpecs.Diameter/2;
-        target.XYZ(3) = targetSpecs.VerticalOffset;
+        target.XYZ(3) = targetSpecs.VerticalBias;
         target.theta = 0;
         target.diameter = targetSpecs.Diameter;
         target.color = targetSpecs.Color;
@@ -82,8 +170,8 @@ switch wall
         turret.theta = target.theta + turretAng;
     case 'NW'
         target.XYZ(1) = -(h.Room_Width/2 - targetSpecs.Diameter/2);
-        target.XYZ(2) = -(targetSpecs.HorizontalOffset);
-        target.XYZ(3) = targetSpecs.VerticalOffset;
+        target.XYZ(2) = -(targetSpecs.HorizontalBias);
+        target.XYZ(3) = targetSpecs.VerticalBias;
         target.theta = pi/2;
         target.diameter = targetSpecs.Diameter;
         target.color = targetSpecs.Color;
@@ -94,9 +182,9 @@ switch wall
         turret.XYZ(3) = 0;
         turret.theta = target.theta + turretAng;
     case 'SW'
-        target.XYZ(1) = -(targetSpecs.HorizontalOffset);
+        target.XYZ(1) = -(targetSpecs.HorizontalBias);
         target.XYZ(2) = -(h.Room_Length/2 - targetSpecs.Diameter/2);
-        target.XYZ(3) = targetSpecs.VerticalOffset;
+        target.XYZ(3) = targetSpecs.VerticalBias;
         target.theta = pi;
         target.diameter = targetSpecs.Diameter;
         target.color = targetSpecs.Color;
@@ -108,8 +196,8 @@ switch wall
         turret.theta = target.theta + turretAng;
     case 'SE'
         target.XYZ(1) = h.Room_Width/2 - targetSpecs.Diameter/2;
-        target.XYZ(2) = targetSpecs.HorizontalOffset;
-        target.XYZ(3) = targetSpecs.VerticalOffset;
+        target.XYZ(2) = targetSpecs.HorizontalBias;
+        target.XYZ(3) = targetSpecs.VerticalBias;
         target.theta = 3*pi/2;
         target.diameter = targetSpecs.Diameter;
         target.color = targetSpecs.Color;
@@ -130,11 +218,3 @@ h = moveTurretFOV(h,turret.theta);
 %% Create and place the target
 h_a2r = createTarget(h.Frames.h_r2b,'Circle',target.diameter,target.color);
 h_a2r = placeTarget(h_a2r,target.XYZ,target.theta,target.wobble);
-
-%% Package Output
-if nargout > 0
-    varargout{1} = h;
-end
-if nargout > 1
-    varargout{2} = h_a2r;
-end
