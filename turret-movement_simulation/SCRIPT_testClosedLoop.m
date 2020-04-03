d425649972cd464cede53e08eb20063ec70608cb
devriesusna <35604535+devriesusna@users.noreply.github.com>
2020-04-02 21:47:09 -0400

    Davis Gagnon Investigation

--- turret-movement_simulation/SCRIPT_testClosedLoop.m
+++ turret-movement_simulation/SCRIPT_testClosedLoop.m
@@ -1,7 +1,7 @@
 cntrlprms.despos = pi/4;
-cntrlprms.Kp = 1.9016;
-cntrlprms.Ki = 0.4992;
-cntrlprms.Kd = 0.73;
+cntrlprms.Kp = 1000;
+cntrlprms.Ki = 100;
+cntrlprms.Kd = 200;
 dt = 0.05;
 t = 0:dt:6;
 [SSE,t,theta,omega,dc,eint] = sendCmdtoDcMotor('closed',cntrlprms,t);
@@ -12,43 +12,46 @@ plot(t,theta*180/pi)
 ylabel('Orientation (degrees)')
 subplot(4,1,2)
 plot(t,omega*180/pi)
-ylabel('Angular Rate (degrees/sec)')
+hold on
+plot(t(1:end-1),diff(theta)/dt*180/pi,'--r')
+legend('ODE45 result','numerical differencing of theta')
+ylabel('Ang. Rate (deg./sec)')
 subplot(4,1,3)
 plot(t(1:end-1),diff(omega)/dt*180/pi)
-ylabel('Angular Acceleration (degrees/sec^2)')
+ylabel('Ang. Acc. (deg./sec^2)')
 subplot(4,1,4)
 plot(t,dc)
 ylabel('Duty Cycle')
 xlabel('Time (s)')
 
 
-svit = 1;
+svit = 0;
 if svit==1
     saveas(gcf,'Testing_figure_CL.png','png')
 end
 
 
 
-cntrlprms.stepPWM = 0.4;
-t = 0:dt:6;
-[SSE,t,theta,omega,dc,eint] = sendCmdtoDcMotor('step',cntrlprms,t);
-
-figure(2); clf
-subplot(4,1,1)
-plot(t,theta*180/pi)
-ylabel('Orientation (deg)')
-subplot(4,1,2)
-plot(t,omega*180/pi)
-ylabel('Rate (deg/sec)')
-subplot(4,1,3)
-plot(t(1:end-1),diff(omega)/dt*180/pi)
-ylabel('Acceleration (deg/sec^2)')
-subplot(4,1,4)
-plot(t,dc)
-ylabel('Duty Cycle')
-xlabel('Time (s)')
-
-
-if svit==1
-    saveas(gcf,'Testing_figure_OL.png','png')
-end
+% cntrlprms.stepPWM = 0.4;
+% t = 0:dt:6;
+% [SSE,t,theta,omega,dc,eint] = sendCmdtoDcMotor('step',cntrlprms,t);
+% 
+% figure(2); clf
+% subplot(4,1,1)
+% plot(t,theta*180/pi)
+% ylabel('Orientation (deg)')
+% subplot(4,1,2)
+% plot(t,omega*180/pi)
+% ylabel('Rate (deg/sec)')
+% subplot(4,1,3)
+% plot(t(1:end-1),diff(omega)/dt*180/pi)
+% ylabel('Acceleration (deg/sec^2)')
+% subplot(4,1,4)
+% plot(t,dc)
+% ylabel('Duty Cycle')
+% xlabel('Time (s)')
+% 
+% 
+% if svit==1
+%     saveas(gcf,'Testing_figure_OL.png','png')
+% end
